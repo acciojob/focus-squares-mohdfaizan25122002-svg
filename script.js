b2029cbbbe9a24a493491bc75e011f9220b4469d
mohdfaizan25122002-svg <mohdfaizan25122002@gmail.com>
2026-02-12 14:50:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-//your JS code here. If required.
-let square=document.querySelectorAll(".sqaure")
+// your JS code here
+let squares = document.querySelectorAll(".square");
 
-for(let  box of squares){
-	box.addEventListener("mouseenter" , hover);
-	box.addEventListener("mouseleave" , reset);
-}
-function hover(event){
-	for(let box of squares){
-       if(box !== event.target){
-           box.style.backgroundColor="#6F4E37"
-	   }
-		else{
-			box.style.backgroundColor="#E6E6FA"
-		}
-	}
-
+for (let box of squares) {
+    box.addEventListener("mouseenter", hover);
+    box.addEventListener("mouseleave", reset);
 }
 
-function reset(){
-	for(let box  of square){
-		box.style.backgroundColor="#E6E6FA"
-	}
+function hover(event) {
+    for (let box of squares) {
+        if (box !== event.target) {
+            box.style.backgroundColor = "#6F4E37"; // Coffee
+        } else {
+            box.style.backgroundColor = "#E6E6FA"; // Lavender
+        }
+    }
+}
+
+function reset() {
+    for (let box of squares) {
+        box.style.backgroundColor = "#E6E6FA"; // Reset to Lavender
+    }
 }
