f1797e875b082b8c60ebb12ab13ba3e55585a618
mohdfaizan25122002-svg <mohdfaizan25122002@gmail.com>
2026-02-12 14:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let square=document.querySelectorAll(".sqaure")
+
+for(let  box of squares){
+	box.addEventListener("mouseenter" , hover);
+	box.addEventListener("mouseleave" , reset);
+}
+function hover(event){
+	for(let box of squares){
+       if(box !== event.target){
+           box.style.backgroundColor="#6F4E37"
+	   }
+		else{
+			box.style.backgroundColor="#E6E6FA"
+		}
+	}
+
+}
+
+function reset(){
+	for(let box  of square){
+		box.style.backgroundColor="#E6E6FA"
+	}
+}
